6479d4ee37241410b2405eb5781234ad71a6b2d4
Samet Candan <c._samet@hotmail.com>
2025-01-17 00:34:57 +0300

    ejs partials

--- app.js
+++ app.js
@@ -2,10 +2,36 @@ const express = require("express");
 
 const app = express();
 
+//TEMPLATE ENGINE
+
+app.set("view engine", "ejs");
+
+//MIDDLEWARES
+
+app.use(express.static("public"));
+
+//ROUTES
+
 app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+
+app.get("/add", (req, res) => {
+  res.render("add_post");
+});
+
+app.get("/post", (req, res) => {
+  res.render("post");
+});
+
+/* app.get("/", (req, res) => {
   const blog = { id: 1, title: "Blog title", description: "Blog description" };
   res.send(blog);
-});
+}); */
 
 const port = 3000;
 
